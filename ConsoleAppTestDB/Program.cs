64e3039b1ad21d02fab271f852309ab9ec090bce
Fred <domaine75@gmail.com>
2019-03-04 19:58:23 +0100

    Add a console app project to test DB access with EF 6.2

---
+++ ConsoleAppTestDB/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleAppTestDB
+{
+  internal class Program
+  {
+    private static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+
+
+      display("press any key to exit:");
+      Console.ReadKey();
+    }
+  }
+}
